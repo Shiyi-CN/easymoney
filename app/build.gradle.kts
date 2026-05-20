529785ff69ec4a264b0f96a325ba09bab32cb3ea
十一 <401959836@qq.com>
2026-05-20 15:01:40 +0800

    v1.1.2: 版本同步，更新日志文档完善

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.jiyixia.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.1.1"
+        versionCode = 5
+        versionName = "1.1.2"
     }
 
     buildTypes {
